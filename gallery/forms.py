2d0fd9e8ec7dc4d85ee268fc0988a2fe16727d52
Faiz Assabil Firdaus <fassabilf@gmail.com>
2024-10-26 12:28:21 +0700

    gallery sisa pagination

--- gallery/forms.py
+++ gallery/forms.py
@@ -1,7 +1,45 @@
 from django import forms
+from django.utils.html import strip_tags
 from gallery.models import GalleryEntry
 
 class GalleryEntryForm(forms.ModelForm):
     class Meta:
         model = GalleryEntry
-        fields = ['nama_batik', 'deskripsi', 'asal_usul', 'makna', 'foto']
+        fields = ['nama_batik', 'deskripsi', 'makna', 'asal_usul', 'foto']
+        widgets = {
+            'nama_batik': forms.TextInput(attrs={
+                'class': 'w-full border-2 border-[#D88E30] rounded-lg p-3',
+                'placeholder': 'Masukkan nama batik'
+            }),
+            'deskripsi': forms.Textarea(attrs={
+                'class': 'w-full border-2 border-[#D88E30] rounded-lg p-3',
+                'placeholder': 'Masukkan deskripsi batik'
+            }),
+            'makna': forms.Textarea(attrs={
+                'class': 'w-full border-2 border-[#D88E30] rounded-lg p-3',
+                'placeholder': 'Masukkan makna batik'
+            }),
+            'asal_usul': forms.Textarea(attrs={
+                'class': 'w-full border-2 border-[#D88E30] rounded-lg p-3',
+                'placeholder': 'Masukkan asal-usul batik'
+            }),
+            'foto': forms.ClearableFileInput(attrs={
+                'class': 'w-full border-2 border-dashed border-[#D88E30] rounded-lg p-3',
+            }),
+        }
+
+    def clean_nama_batik(self):
+        nama_batik = self.cleaned_data.get("nama_batik", "")
+        return strip_tags(nama_batik)  # Remove HTML tags
+
+    def clean_deskripsi(self):
+        deskripsi = self.cleaned_data.get("deskripsi", "")
+        return strip_tags(deskripsi)  # Remove HTML tags
+
+    def clean_makna(self):
+        makna = self.cleaned_data.get("makna", "")
+        return strip_tags(makna)  # Remove HTML tags
+
+    def clean_asal_usul(self):
+        asal_usul = self.cleaned_data.get("asal_usul", "")
+        return strip_tags(asal_usul)  # Remove HTML tags
--- gallery/urls.py
+++ gallery/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
-from .views import show_gallery, add_gallery_entry, edit_gallery_entry, delete_gallery_entry
+from .views import (
+    show_gallery, add_gallery_entry, edit_gallery_entry, delete_gallery_entry,
+    show_gallery_xml, show_gallery_json, show_gallery_xml_by_id, show_gallery_json_by_id,
+    add_gallery_entry_ajax
+)
 
 app_name = 'gallery'
 
 urlpatterns = [
     path('', show_gallery, name='show_gallery'),
     path('add/', add_gallery_entry, name='add_gallery_entry'),
-    path('edit/<uuid:id>/', edit_gallery_entry, name='edit_gallery_entry'),  # UUID karena id adalah UUIDField
+    path('edit/<uuid:id>/', edit_gallery_entry, name='edit_gallery_entry'),
     path('delete/<uuid:id>/', delete_gallery_entry, name='delete_gallery_entry'),
+    path('xml/', show_gallery_xml, name='show_gallery_xml'),
+    path('json/', show_gallery_json, name='show_gallery_json'),
+    path('xml/<uuid:id>/', show_gallery_xml_by_id, name='show_gallery_xml_by_id'),
+    path('json/<uuid:id>/', show_gallery_json_by_id, name='show_gallery_json_by_id'),
+    path('add/ajax/', add_gallery_entry_ajax, name='add_gallery_entry_ajax'),  # AJAX endpoint
 ]
