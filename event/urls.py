c1f6356a0c75e47dfec94039adb3d45b8bfd3b0f
FrankyRay <frankyraymarcellsinaga@gmail.com>
2024-10-27 01:15:07 +0700

    feat: create and edit

--- event/urls.py
+++ event/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from event.views import show_event, create_event
+from event.views import show_event, create_event, edit_event
 
 app_name = "event"
 
 urlpatterns = [
     path("", show_event, name="show_event"),
     path("create", create_event, name="create_event"),
+    path("edit/<uuid:id>", edit_event, name="edit_event"),
 ]
